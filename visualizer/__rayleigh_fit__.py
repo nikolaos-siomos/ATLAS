Final refinements before releash of 0.4.4 -- The Rayleigh fit now can automatically calculate the uppermost acceptable normalization region. Plots of the applied masks are generated -- Corrected a bag in the smoothing_fast routines. Now smotthing takes place also within the buffer zones. If the smoothing window is too larges and gets out of the array borders, then it is adjusted accordingly to fit -- Fixed a bug that resulted to a wrong radiosonde date for the pol. cal. plot -- The cal. factor box is now green when the rel. error is < 2% and red if it is above -- The delta_res is now correctly calculated from the delta_cor (after GHK correction) and the delta_mol -- The telecover legend is now placed in a better position -- A bug concerning the extra sector was fixed. If the extra sector is not provided, the alorithm no longer crashes -- The N-Nx curve was wrongly calculated as the absolute bias of the two lines. Now it is correctly showing the relative bias
To Do: Add settings for the automated Rayleigh fit

visualizer/__rayleigh_fit__.py
@@ -106,29 +106,6 @@ def main(args, __version__):
             y_vals_sm = sig_ch.copy()
             y_errs = np.nan * y_vals_sm
         
-        # # Normalization of the signals
-        # coef_c, n_bin = normalize.to_a_point(sig = y_vals_sm, 
-        #                                      sig_b = atb_ch.copy(), 
-        #                                      x_vals = x_vals,
-        #                                      region = args['normalization_region'],
-        #                                      axis = 0)
-        
-        # _, _, sem_c = average.region(sig = sig_ch.copy(), 
-        #                              x_vals = x_vals, 
-        #                              region = args['normalization_region'],
-        #                              axis = 0, 
-        #                              squeeze = True)
-        
-        # atb_c, _, _ = average.region(sig = atb_ch.copy(), 
-        #                              x_vals = x_vals, 
-        #                              region = args['normalization_region'],
-        #                              axis = 0, 
-        #                              squeeze = True)
-        
-        # mder, sder, pval = differentiate.region(sig = coef_c[0] * sig_ch.copy() - atb_c,
-        #                                         x_vals = x_vals, 
-        #                                         region = args['normalization_region'],
-        #                                         axis = 0)
         
         nder, mder, cder, rerr, rsem, mshp, coef, mneg = \
             curve_fit.stats(y1 = sig_ch.copy(),
@@ -177,6 +154,7 @@ def main(args, __version__):
                                     rs_start_time = rs_start_time, 
                                     wmo_id = wmo_id,
                                     wban_id = wban_id)
+        
     
         # Make plot filename
         fname = f'{data.Measurement_ID}_{data.Lidar_Name}_ray_{ch}_ATLAS_{__version__}.png'
@@ -230,79 +208,15 @@ def main(args, __version__):
                               atb = atb_ch, 
                               rcs = sig_ch, 
                               header = header)
-
-        from matplotlib import pyplot as plt
-        [X, Y] = np.meshgrid(mmol.lower_limit.values, mmol.window)
-        fig = plt.figure(figsize=(12. , 8.))
-        fig.suptitle(title)
-        
-        x_llim = 0.
-        x_ulim = 16.
-        
-        y_llim = 1.
-        y_ulim = 4.
-        
-        fig_x = 0.42
-        fig_y = 0.23
-        
-        fig_edg1_x = 0.07
-        fig_edg2_x = 0.53
-        
-        fig_edg1_y = 0.07
-        fig_edg2_y = 0.36
-        fig_edg3_y = 0.65
-        
-        ax = fig.add_axes([fig_edg1_x, fig_edg3_y, fig_x, fig_y])
-        ax.pcolormesh(X, Y, mder.values)
-        ax.set_title('Derivative mask', pad = 5)
-        ax.set_ylabel('Window [km]')
-        ax.set_ylim([y_llim, y_ulim])
-        ax.set_xlim([x_llim, x_ulim])
-
-        ax2 = fig.add_axes([fig_edg2_x, fig_edg3_y, fig_x, fig_y])
-        ax2.pcolormesh(X, Y, rsem.values <= 0.02)
-        ax2.set_title('Relative SEM mask', pad = 5)
-        ax2.set_ylim([y_llim, y_ulim])
-        ax2.set_xlim([x_llim, x_ulim])
-        
-        ax3 = fig.add_axes([fig_edg1_x, fig_edg2_y, fig_x, fig_y])
-        ax3.pcolormesh(X, Y, cder.values)
-        ax3.set_title('Curvature mask', pad = 5)
-        ax3.set_ylabel('Window [km]')
-        ax3.set_ylim([y_llim, y_ulim])
-        ax3.set_xlim([x_llim, x_ulim])       
-
-        ax4 = fig.add_axes([fig_edg2_x, fig_edg2_y, fig_x, fig_y])
-        ax4.pcolormesh(X, Y, mshp.values)
-        ax4.set_title('Shapiro-Wilk mask', pad = 5)
-        ax4.set_ylim([y_llim, y_ulim])
-        ax4.set_xlim([x_llim, x_ulim])
-
-        ax5 = fig.add_axes([fig_edg1_x, fig_edg1_y, fig_x, fig_y])
-        ax5.pcolormesh(X, Y, mneg.values)
-        ax5.set_title('Cross-check mask', pad = 5)
-        ax5.set_xlabel('Lower Limit [km]')
-        ax5.set_ylabel('Window [km]')
-        ax5.set_ylim([y_llim, y_ulim])
-        ax5.set_xlim([x_llim, x_ulim])
-
-        ax6 = fig.add_axes([0.53, fig_edg1_y, fig_x, fig_y])
-        ax6.pcolormesh(X, Y, rsem.where(mmol).values)
-        ax6.set_title('Masked Relative SEM', pad = 5)
-        ax6.set_xlabel('Lower Limit [km]')
-        ax6.set_ylim([y_llim, y_ulim])
-        ax6.set_xlim([x_llim, x_ulim])
         
         fname_mask = f'{data.Measurement_ID}_{data.Lidar_Name}_ray_{ch}_mask_ATLAS_{__version__}.png'
-    
-        import os
-        fpath = os.path.join(args['output_folder'], 'plots', fname_mask)
 
-        fig.savefig(fpath, dpi = args['dpi'])
-        
-        fig.clf()
-        
-        plt.close()
+        make_plot.rayleigh_mask(dir_out = args['output_folder'], 
+                                fname = fname_mask, title = title,
+                                dpi_val = args['dpi'],
+                                color_reduction = args['color_reduction'],
+                                mmol = mmol, mder = mder, cder = cder, 
+                                mshp = mshp, mneg = mneg, rsem = rsem)
         
         # Add metadata to the quicklook plot
         from PIL import Image

visualizer/plotting/make_plot.py
@@ -265,6 +265,93 @@ def rayleigh(dir_out, fname, title, dpi_val, color_reduction, use_lin, norm_regi
     
     return(fpath)
 
+def rayleigh_mask(dir_out, fname, title, dpi_val, color_reduction,
+                  mmol, mder, cder, mshp, mneg, rsem):
+
+    rgb = color_lib.volkers_rgb()
+    vlk_cmap = make_colormap.custom_rgb(rgb, name = 'volkers')
+
+    [X, Y] = np.meshgrid(mmol.lower_limit.values, mmol.window)
+    fig = plt.figure(figsize=(12. , 8.))
+    fig.suptitle(title)
+    
+    x_llim = 0.
+    x_ulim = 16.
+    
+    y_llim = 1.
+    y_ulim = 4.
+    
+    fig_x = 0.44
+    fig_y = 0.23
+    
+    fig_edg1_x = 0.06
+    fig_edg2_x = 0.54
+    
+    fig_edg1_y = 0.07
+    fig_edg2_y = 0.36
+    fig_edg3_y = 0.65
+    
+    ax = fig.add_axes([fig_edg1_x, fig_edg3_y, fig_x, fig_y])
+    ax.pcolormesh(X, Y, mder.values)
+    ax.set_title('Derivative mask', pad = 5)
+    ax.set_ylabel('Window [km]')
+    ax.set_ylim([y_llim, y_ulim])
+    ax.set_xlim([x_llim, x_ulim])
+
+    ax2 = fig.add_axes([fig_edg2_x, fig_edg3_y, fig_x, fig_y])
+    ax2.pcolormesh(X, Y, rsem.values <= 0.02)
+    ax2.set_title('Relative SEM mask', pad = 5)
+    ax2.set_ylim([y_llim, y_ulim])
+    ax2.set_xlim([x_llim, x_ulim])
+    
+    ax3 = fig.add_axes([fig_edg1_x, fig_edg2_y, fig_x, fig_y])
+    ax3.pcolormesh(X, Y, cder.values)
+    ax3.set_title('Curvature mask', pad = 5)
+    ax3.set_ylabel('Window [km]')
+    ax3.set_ylim([y_llim, y_ulim])
+    ax3.set_xlim([x_llim, x_ulim])       
+
+    ax4 = fig.add_axes([fig_edg2_x, fig_edg2_y, fig_x, fig_y])
+    ax4.pcolormesh(X, Y, mshp.values)
+    ax4.set_title('Shapiro-Wilk mask', pad = 5)
+    ax4.set_ylim([y_llim, y_ulim])
+    ax4.set_xlim([x_llim, x_ulim])
+
+    ax5 = fig.add_axes([fig_edg1_x, fig_edg1_y, fig_x, fig_y])
+    ax5.pcolormesh(X, Y, mneg.values)
+    ax5.set_title('Cross-check mask', pad = 5)
+    ax5.set_xlabel('Lower Limit [km]')
+    ax5.set_ylabel('Window [km]')
+    ax5.set_ylim([y_llim, y_ulim])
+    ax5.set_xlim([x_llim, x_ulim])
+
+    ax6 = fig.add_axes([fig_edg2_x, fig_edg1_y, fig_x, fig_y])
+    plot6 = ax6.pcolormesh(X, Y, 100. * rsem.where(mmol).values, vmin = 0., vmax = 2., cmap = vlk_cmap)
+    ax6.set_title('Masked Relative SEM (%)', pad = 5)
+    ax6.grid(which = 'both')
+    ax6.axes.xaxis.set_minor_locator(MultipleLocator(1))
+    ax6.set_xlabel('Lower Limit [km]')
+    ax6.set_ylim([y_llim, y_ulim])
+    ax6.set_xlim([x_llim, x_ulim])
+    
+    cax = fig.add_axes([fig_edg2_x + 0.01, fig_edg1_y +0.01, 0.01, fig_y -0.02])
+
+    fig.colorbar(plot6, cax=cax, orientation='vertical')
+    
+    fpath = os.path.join(dir_out, 'plots', fname)
+
+    fig.savefig(fpath, dpi = dpi_val)
+    
+    fig.clf()
+    
+    plt.close()
+    
+    if color_reduction == True:
+        run(["convert", fpath, "-posterize", "8", fpath])
+    
+    return(fpath)
+
+
 def telecover_sec(dir_out, fname, title, dpi_val, color_reduction, 
                   norm_region, use_nonrc, x_vals, 
                   y1_raw, y2_raw, y3_raw, y4_raw,
@@ -406,20 +493,23 @@ def telecover_sec(dir_out, fname, title, dpi_val, color_reduction,
         ax.plot(X, Y_E, color = 'tab:purple', label = extra_label, alpha = 0.7)
         
     ax.legend()
+
+    ax.plot([X[0], X[-1]], [0., 0.], color = 'black')
     
     ax.set_xticks(x_ticks, labels = x_ticks)
     ax.set_xlim([x_llim, x_ulim])
     ax.set_xlabel(x_label, loc = "right")
     
-    raw_llim = np.nanmin([Y1[x_lbin:x_ubin], Y2[x_lbin:x_ubin],
-                          Y3[x_lbin:x_ubin], Y4[x_lbin:x_ubin]])
+    # raw_llim = np.nanmin([Y1[x_lbin:x_ubin], Y2[x_lbin:x_ubin],
+    #                       Y3[x_lbin:x_ubin], Y4[x_lbin:x_ubin]])
     raw_ulim = np.nanmax([Y1[x_lbin:x_ubin], Y2[x_lbin:x_ubin],
                           Y3[x_lbin:x_ubin], Y4[x_lbin:x_ubin]])
     
-    if raw_llim > 0.:
-        ax.set_ylim([0, 1.1 * raw_ulim])
-    else:
-        ax.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+    # if raw_llim > 0.:
+    #     ax.set_ylim([0, 1.1 * raw_ulim])
+    # else:
+    #     ax.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+    ax.set_ylim([-0.1*raw_ulim, 1.1 * raw_ulim])
 
     ax.set_ylabel(y_label_1)
     ax.ticklabel_format(axis = 'y', useMathText = True, style='sci', scilimits=(-1,1))
@@ -437,14 +527,17 @@ def telecover_sec(dir_out, fname, title, dpi_val, color_reduction,
     if use_last == True and extra_exists:
         ax2.plot(X, Y_E, color = 'tab:purple', alpha = 0.7)
                 
+    ax2.plot([X[0], X[-1]], [0., 0.], color = 'black')
+
     ax2.set_xlim([x_ulim, 20.])
     # ax2.set_xlabel(x_label)
     ax2.xaxis.set_minor_locator(MultipleLocator(2.))
 
-    if raw_llim > 0.:
-        ax2.set_ylim([0, 1.1 * raw_ulim])
-    else:
-        ax2.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+    # if raw_llim > 0.:
+    #     ax2.set_ylim([0, 1.1 * raw_ulim])
+    # else:
+    #     ax2.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+    ax2.set_ylim([-0.1*raw_ulim, 1.1 * raw_ulim])
 
     ax2.set(yticklabels=[])
 
@@ -464,6 +557,8 @@ def telecover_sec(dir_out, fname, title, dpi_val, color_reduction,
     ax3.fill_between(X, Y2_NL, Y2_NU, color = 'tab:orange', alpha = 0.3)
     ax3.fill_between(X, Y3_NL, Y3_NU, color = 'tab:green', alpha = 0.3)
     ax3.fill_between(X, Y4_NL, Y4_NU, color = 'tab:red', alpha = 0.3)
+
+    ax3.plot([X[0], X[-1]], [0., 0.], color = 'black')
 
     ax3.set_xticks(x_ticks, labels = x_ticks)
     ax3.set_xlim([x_llim, x_ulim])
@@ -498,6 +593,8 @@ def telecover_sec(dir_out, fname, title, dpi_val, color_reduction,
     ax4.fill_between(X, Y2_NL, Y2_NU, color = 'tab:orange', alpha = 0.3)
     ax4.fill_between(X, Y3_NL, Y3_NU, color = 'tab:green', alpha = 0.3)
     ax4.fill_between(X, Y4_NL, Y4_NU, color = 'tab:red', alpha = 0.3)
+
+    ax4.plot([X[0], X[-1]], [0., 0.], color = 'black')
 
     ax4.set_xlim([x_ulim, 20.])
     # ax4.set_xlabel(x_label)
@@ -673,17 +770,20 @@ def telecover_rin(dir_out, fname, title, dpi_val, color_reduction,
         
     ax.legend()
     
+    ax.plot([X[0], X[-1]], [0., 0.], color = 'black')
+
     ax.set_xticks(x_ticks, labels = x_ticks)
     ax.set_xlim([x_llim, x_ulim])
     ax.set_xlabel(x_label, loc = "right")
     
-    raw_llim = np.nanmin([Y1[x_lbin:x_ubin], Y2[x_lbin:x_ubin]])
+    # raw_llim = np.nanmin([Y1[x_lbin:x_ubin], Y2[x_lbin:x_ubin]])
     raw_ulim = np.nanmax([Y1[x_lbin:x_ubin], Y2[x_lbin:x_ubin]])
     
-    if raw_llim > 0.:
-        ax.set_ylim([0, 1.1 * raw_ulim])
-    else:
-        ax.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+    # if raw_llim > 0.:
+    #     ax.set_ylim([0, 1.1 * raw_ulim])
+    # else:
+    #     ax.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+    ax.set_ylim([-0.1*raw_ulim, 1.1 * raw_ulim])
 
     ax.set_ylabel(y_label_1)
     ax.ticklabel_format(axis = 'y', useMathText = True, style='sci', scilimits=(-1,1))
@@ -699,14 +799,18 @@ def telecover_rin(dir_out, fname, title, dpi_val, color_reduction,
     if use_last == True and extra_exists:
         ax2.plot(X, Y_E, color = 'tab:purple', alpha = 0.7)
                 
+    ax2.plot([X[0], X[-1]], [0., 0.], color = 'black')
+    
     ax2.set_xlim([x_ulim, 20.])
     # ax2.set_xlabel(x_label)
     ax2.xaxis.set_minor_locator(MultipleLocator(2.))
 
-    if raw_llim > 0.:
-        ax2.set_ylim([0, 1.1 * raw_ulim])
-    else:
-        ax2.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+    # if raw_llim > 0.:
+    #     ax2.set_ylim([0, 1.1 * raw_ulim])
+    # else:
+    #     ax2.set_ylim([1.1 * raw_llim, 1.1 * raw_ulim])
+
+    ax2.set_ylim([-0.1*raw_ulim, 1.1 * raw_ulim])
 
     ax2.set(yticklabels=[])
 
@@ -722,6 +826,8 @@ def telecover_rin(dir_out, fname, title, dpi_val, color_reduction,
         
     ax3.fill_between(X, Y1_NL, Y1_NU, color = 'tab:green', alpha = 0.3)
     ax3.fill_between(X, Y2_NL, Y2_NU, color = 'tab:orange', alpha = 0.3)
+
+    ax3.plot([X[0], X[-1]], [0., 0.], color = 'black')
 
     ax3.set_xticks(x_ticks, labels = x_ticks)
     ax3.set_xlim([x_llim, x_ulim])
@@ -753,6 +859,8 @@ def telecover_rin(dir_out, fname, title, dpi_val, color_reduction,
     ax4.fill_between(X, Y1_NL, Y1_NU, color = 'tab:green', alpha = 0.3)
     ax4.fill_between(X, Y2_NL, Y2_NU, color = 'tab:orange', alpha = 0.3)
     
+    ax4.plot([X[0], X[-1]], [0., 0.], color = 'black')
+
     ax4.set_xlim([x_ulim, 20.])
     # ax4.set_xlabel(x_label)
     ax4.xaxis.set_minor_locator(MultipleLocator(2.))

visualizer/tools/sector.py
@@ -10,6 +10,8 @@ import numpy as np
 from . import normalize
 from ..tools.smoothing import sliding_average_1D_fast as smooth_1D_fast
 from ..tools.smoothing import sliding_average_2D_fast as smooth_2D_fast            
+from ..tools.smoothing import sliding_average_1D as smooth_1D_slow
+from ..tools.smoothing import sliding_average_2D as smooth_2D_slow            
 
 def process(x, y, iters, smooth, x_sm_lims, x_sm_win, expo, region):
 
@@ -24,23 +26,137 @@ def process(x, y, iters, smooth, x_sm_lims, x_sm_win, expo, region):
         else:
             from ..tools.smoothing import sliding_average_1D as smooth_1D
             from ..tools.smoothing import sliding_average_2D as smooth_2D
-    
-        # Smoothing averaged sectors
+
+        # Smoothing averaged sectors        
         y_m_sm_n, _ = \
             smooth_1D(y_vals = y_m, 
                       x_vals = x,
                       x_sm_lims = x_sm_lims,
                       x_sm_win = x_sm_win,
                       expo = expo)
-  
+
         y_m_sm_f, _ = \
             smooth_1D_fast(y_vals = y_m, 
                            x_vals = x,
-                           x_sm_lims = [x_sm_lims[0] - 2E-3* x_sm_win, 20.250],
+                           x_sm_lims = [x_sm_lims[1], 20.],
                            x_sm_win = 500.,
-                           expo = expo)
+                           expo = False)
+
+        # Smoothing unaveraged sectors            
+        y_sm_n = \
+            smooth_2D(z_vals = y, 
+                      y_vals = x,
+                      y_sm_lims = x_sm_lims,
+                      y_sm_win = x_sm_win,
+                      expo = expo)
+
+
+        y_sm_f = \
+            smooth_2D_fast(z_vals = y, 
+                           y_vals = x,
+                           y_sm_lims = [x_sm_lims[1], 20.],
+                           y_sm_win = 500.,
+                           expo = False)
+            
+        y_m_sm = np.nan * np.zeros(y_m.copy().shape)
+         
+        mask_n = (x >  x_sm_lims[0]) & (x <  x_sm_lims[1])       
+        mask_f = (x >= x_sm_lims[1]) & (x < 20.)
+
+        y_m_sm[mask_n] = y_m_sm_n[mask_n]
+        y_m_sm[mask_f] = y_m_sm_f[mask_f]
+
+        y_sm = y.copy()
+        
+        y_sm[:, mask_n] = y_sm_n[:, mask_n]
+        y_sm[:, mask_f] = y_sm_f[:, mask_f]
+        
+    else:
+        y_m_sm = y_m
+        
+        y_sm = y
+
+    # Store the extra iteration if it exists 
+    if y_sm.shape[0] == iters + 1:
+        y_extra = y[iters,:]
+        y_extra_sm = y_sm[iters,:]
+        extra = True
+    else:
+        y_extra = []
+        y_extra_sm = []
+        coef_extra = []
+        extra = False 
+
+        
+    # Minimum value per bin of all iterations
+    y_l_sm = np.nanmin(y_sm[:iters,:], axis = 0)
+
+    # Maximum value per bin of all iterations
+    y_u_sm = np.nanmax(y_sm[:iters,:], axis = 0)
+
+    # Normalization for smoothed signals
+    coef, norm_bin = normalize.to_a_point(sig = y_m, 
+                                          sig_b = np.ones(x.shape), 
+                                          x_vals = x,
+                                          region = region,
+                                          axis = 0)
+
+    if extra:
+        coef_extra, norm_bin_extra = \
+            normalize.to_a_point(sig = y_extra, 
+                                 sig_b = np.ones(x.shape), 
+                                 x_vals = x,
+                                 region = region,
+                                 axis = 0)    
+            
+    return(coef, y_m, y_sm, y_m_sm, y_l_sm, y_u_sm, coef_extra, y_extra, y_extra_sm, extra)
+
+def process_old(x, y, iters, smooth, x_sm_lims, x_sm_win, expo, region):
+
+    # Averaging sector signals
+    y_m = np.nanmean(y[:iters,:], axis = 0) 
+
+    # Smoothing
+    if smooth:
+        if not isinstance(x_sm_win,list):
+            from ..tools.smoothing import sliding_average_1D_fast as smooth_1D
+            from ..tools.smoothing import sliding_average_2D_fast as smooth_2D
+        else:
+            from ..tools.smoothing import sliding_average_1D as smooth_1D
+            from ..tools.smoothing import sliding_average_2D as smooth_2D
+    
+        if not isinstance(x_sm_win,list):
+            # Smoothing averaged sectors
+            y_m_hsm_n, _ = \
+                smooth_1D_slow(y_vals = y_m, 
+                               x_vals = x,
+                               x_sm_lims = [0.,x_sm_lims[0] + x_sm_win],
+                               x_sm_win = [x_sm_win, x_sm_win],
+                               expo = False)
+        
+        y_m_sm_n, _ = \
+            smooth_1D(y_vals = y_m, 
+                      x_vals = x,
+                      x_sm_lims = x_sm_lims,
+                      x_sm_win = x_sm_win,
+                      expo = expo)
+
+        y_m_sm_f, _ = \
+            smooth_1D_fast(y_vals = y_m, 
+                           x_vals = x,
+                           x_sm_lims = [0., 20.250],
+                           x_sm_win = 500.,
+                           expo = False)
 
         # Smoothing unaveraged sectors
+        if not isinstance(x_sm_win,list):
+            y_hsm_n = \
+                smooth_2D_slow(z_vals = y, 
+                               y_vals = x,
+                               y_sm_lims = [0.,x_sm_lims[0] + x_sm_win],
+                               y_sm_win = [x_sm_win, x_sm_win],
+                               expo = False)
+            
         y_sm_n = \
             smooth_2D(z_vals = y, 
                       y_vals = x,
@@ -51,25 +167,29 @@ def process(x, y, iters, smooth, x_sm_lims, x_sm_win, expo, region):
         y_sm_f = \
             smooth_2D_fast(z_vals = y, 
                            y_vals = x,
-                           y_sm_lims = [x_sm_lims[0] - 2E-3* x_sm_win,20.250],
+                           y_sm_lims = [0.,20.250],
                            y_sm_win = 500.,
-                           expo = expo)
+                           expo = False)
             
         y_m_sm = np.nan * y_m
         
-        mask_n = (x <  x_sm_lims[1])
+        if not isinstance(x_sm_win,list):
+            mask_s = (x <=  x_sm_lims[0] + x_sm_win)
+            mask_n = (x <  x_sm_lims[1]) & (x >  x_sm_lims[0] + x_sm_win)
+        else:
+            mask_n = (x <  x_sm_lims[1])
+            
         mask_f = (x >= x_sm_lims[1]) & (x < 20.)
         
-        # from matplotlib import pyplot as plt
-        # plt.plot()
-        # print(x_sm_lims[1] - 1E-3 * x_sm_win/2.)
-        # raise Exception()
-
+        if not isinstance(x_sm_win,list):
+            y_m_sm[mask_s] = y_m_hsm_n[mask_s]
         y_m_sm[mask_n] = y_m_sm_n[mask_n]
         y_m_sm[mask_f] = y_m_sm_f[mask_f]
 
         y_sm = np.nan * y
         
+        if not isinstance(x_sm_win,list):
+            y_sm[:, mask_s] = y_hsm_n[:, mask_s]
         y_sm[:, mask_n] = y_sm_n[:, mask_n]
         y_sm[:, mask_f] = y_sm_f[:, mask_f]
         

visualizer/tools/smoothing.py
@@ -11,7 +11,7 @@ import numpy as np
 def sliding_average_2D(z_vals, y_vals, y_sm_lims, y_sm_win, expo = False):
     
     s_bin = np.where(y_vals >= y_sm_lims[0])[0][0]
-    e_bin = np.where(y_vals <= y_sm_lims[-1])[0][-1]
+    e_bin = np.where(y_vals <= y_sm_lims[-1])[0][-1] + 1
     s_ihwin = int(1E-3 * y_sm_win[0]  / (2. * (y_vals[1] - y_vals[0])))
     e_ihwin = int(1E-3 * y_sm_win[-1] / (2. * (y_vals[1] - y_vals[0])))
 
@@ -51,11 +51,90 @@ def sliding_average_2D(z_vals, y_vals, y_sm_lims, y_sm_win, expo = False):
     
     return(z_vals_sm)
 
+def sliding_average_2D_fast(z_vals, y_vals, y_sm_lims, y_sm_win, expo = None, err_type = 'sem'):
+    
+    win = int(1E-3 * y_sm_win  / (y_vals[1] - y_vals[0]))
+    
+    if win % 2 == 0: win = win + 1
+    
+    s_bin = np.where(y_vals >= y_sm_lims[0])[0][0]
+    e_bin = np.where(y_vals <= y_sm_lims[1])[0][-1] + 1
+    
+    if s_bin >= e_bin:
+        raise Exception(f"The smoothing region {y_sm_lims} is wrongly set. Please provide a lower limit that is lower than the higher limit")
+    
+    buf = int(np.floor(win / 2.))
+
+    s_buf = s_bin + buf
+    e_buf = e_bin - buf
+    
+    if s_buf >= e_buf:
+        raise Exception(f"The smoothing region {y_sm_lims} is too narrow set. Please provide a region that is at least as wide as the smoothing windo {win}.")
+    
+    z_avg = z_vals.copy()
+    z_err = np.nan * np.zeros(z_vals.copy().shape)
+    z_sqr = np.power(z_vals.copy(), 2)
+
+    # z_vals.copy()
+    # del z_vals
+
+    for j in range(z_avg.shape[0]):
+        z_avg[j,s_buf:e_buf] = np.convolve(z_vals[j,s_bin:e_bin], np.ones(win), 'valid') / win
+        
+    if err_type == 'sem':
+        z_err[:, s_buf:e_buf] = np.sqrt(z_sqr[:,s_buf:e_buf] - np.power(z_vals[:,s_buf:e_buf], 2)) / np.sqrt(win)
+    elif err_type == 'std':
+        z_err[:, s_buf:e_buf] = np.sqrt(z_sqr[:,s_buf:e_buf] - np.power(z_vals[:,s_buf:e_buf], 2))
+    else:
+        raise Exception('Error type provided is not understoud. Please select one of: sem, std')
+
+    del z_sqr
+    
+    for i in range(s_bin, s_buf):
+        
+        if i <= buf:
+            ihwin = int(i)
+        
+        else:
+            ihwin = buf
+            
+        if ihwin > 0 and not np.isnan(ihwin):
+            z_avg[:,i] = np.nanmean(z_vals[:,i-ihwin:i+ihwin+1], axis = 1)
+            
+            if err_type == 'sem':
+                z_err[:,i] = np.nanstd(z_vals[:,i-ihwin:i+ihwin+1]) /\
+                    np.sqrt(z_vals[:,i-ihwin:i+ihwin+1].size)
+            elif err_type == 'std':
+                z_err[:,i] = np.nanstd(z_vals[:,i-ihwin:i+ihwin+1], axis = 1)
+            else:
+                raise Exception('Error type provided is not understoud. Please select one of: sem, std')
+
+    for i in range(e_buf, e_bin):
+        
+        if y_vals.size - i < buf:
+            ihwin = int(y_vals.size - i)
+        
+        else:
+            ihwin = buf
+            
+        if ihwin > 0 and not np.isnan(ihwin):
+            z_avg[:,i] = np.nanmean(z_vals[:,i-ihwin:i+ihwin+1], axis = 1)
+            
+            if err_type == 'sem':
+                z_err[:,i] = np.nanstd(z_vals[:,i-ihwin:i+ihwin+1]) /\
+                    np.sqrt(z_vals[:,i-ihwin:i+ihwin+1].size)
+            elif err_type == 'std':
+                z_err[:,i] = np.nanstd(z_vals[:,i-ihwin:i+ihwin+1], axis = 1)
+            else:
+                raise Exception('Error type provided is not understoud. Please select one of: sem, std')
+    
+    return(z_avg)
+
 def sliding_average_1D(y_vals, x_vals, x_sm_lims, x_sm_win, 
                        expo = False, err_type = 'sem'):
 
     s_bin = np.where(x_vals >= x_sm_lims[0])[0][0]
-    e_bin = np.where(x_vals <= x_sm_lims[-1])[0][-1]
+    e_bin = np.where(x_vals <= x_sm_lims[-1])[0][-1] + 1
     s_ihwin = int(1E-3 * x_sm_win[0]  / (2.*(x_vals[1] - x_vals[0])))
     e_ihwin = int(1E-3 * x_sm_win[-1] / (2.*(x_vals[1] - x_vals[0])))
     
@@ -99,28 +178,34 @@ def sliding_average_1D(y_vals, x_vals, x_sm_lims, x_sm_win,
 
     return(y_vals_sm, y_vals_err)
 
-def sliding_average_1D_fast(y_vals, x_vals, x_sm_lims, x_sm_win, expo = None, err_type = 'sem', buf = []):
+def sliding_average_1D_fast(y_vals, x_vals, x_sm_lims, x_sm_win, expo = None, err_type = 'sem'):
     
     win = int(1E-3 * x_sm_win  / (x_vals[1] - x_vals[0]))
     
     if win % 2 == 0: win = win + 1
     
     s_bin = np.where(x_vals >= x_sm_lims[0])[0][0]
-    e_bin = np.where(x_vals <= x_sm_lims[1])[0][-1]
+    e_bin = np.where(x_vals <= x_sm_lims[1])[0][-1] + 1
     
-    if isinstance(buf, list):
-        buf = int(np.floor(win / 2.))
+    if s_bin >= e_bin:
+        raise Exception(f"The smoothing region {x_sm_lims} is wrongly set. Please provide a lower limit that is lower than the higher limit")
+    
+    buf = int(np.floor(win / 2.))
 
     s_buf = s_bin + buf
     e_buf = e_bin - buf
     
-    y_avg = y_vals.copy()
-    del y_vals
+    if s_buf >= e_buf:
+        raise Exception(f"The smoothing region {x_sm_lims} is too narrow set. Please provide a region that is at least as wide as the smoothing windo {win}.")
     
-    y_sqr = np.power(y_avg, 2)
-    y_err = np.nan * np.zeros(y_avg.shape)
+    y_avg = np.nan * np.zeros(y_vals.shape)
+    y_err = np.nan * np.zeros(y_vals.shape)
+    y_sqr = np.power(y_vals.copy(), 2)
 
-    y_avg[s_buf:e_buf] = np.convolve(y_avg[s_bin:e_bin], np.ones(win), 'valid') / win
+    # y_vals.copy()
+    # del y_vals
+
+    y_avg[s_buf:e_buf] = np.convolve(y_vals[s_bin:e_bin], np.ones(win), 'valid') / win
     y_sqr[s_buf:e_buf] = np.convolve(y_sqr[s_bin:e_bin], np.ones(win), 'valid') / win
     
     if err_type == 'sem':
@@ -134,45 +219,47 @@ def sliding_average_1D_fast(y_vals, x_vals, x_sm_lims, x_sm_win, expo = None, er
     
     for i in range(s_bin, s_buf):
         
-        if buf > i:
-            ihwin = i
-
+        if i <= buf:
+            ihwin = int(i)
+        
         else:
             ihwin = buf
-
         if ihwin > 0 and not np.isnan(ihwin):
-            y_avg[i] = np.nanmean(y_avg[i-int(ihwin):i+int(ihwin)+1])
+            y_avg[i] = np.nanmean(y_vals[i-ihwin:i+ihwin+1])
             
             if err_type == 'sem':
-                y_err[i] = np.nanstd(y_avg[i-int(ihwin):i+int(ihwin)+1]) /\
-                    np.sqrt(y_avg[i-int(ihwin):i+int(ihwin)+1].size)
+                y_err[i] = np.nanstd(y_vals[i-ihwin:i+ihwin+1]) /\
+                    np.sqrt(y_vals[i-ihwin:i+ihwin+1].size)
             elif err_type == 'std':
-                y_err[i] = np.nanstd(y_avg[i-int(ihwin):i+int(ihwin)+1])
+                y_err[i] = np.nanstd(y_vals[i-ihwin:i+ihwin+1])
             else:
                 raise Exception('Error type provided is not understoud. Please select one of: sem, std')
 
     for i in range(e_buf, e_bin):
         
-        if buf > x_vals.size - e_bin :
-            ihwin = x_vals.size - i
-
+        if x_vals.size - i <= buf:
+            ihwin = int(x_vals.size - i)
+        
         else:
             ihwin = buf
-
+            
         if ihwin > 0 and not np.isnan(ihwin):
-            y_avg[i] = np.nanmean(y_avg[i-int(ihwin):i+int(ihwin)+1])
+            y_avg[i] = np.nanmean(y_vals[i-ihwin:i+ihwin+1])
             
             if err_type == 'sem':
-                y_err[i] = np.nanstd(y_avg[i-int(ihwin):i+int(ihwin)+1]) /\
-                    np.sqrt(y_avg[i-int(ihwin):i+int(ihwin)+1].size)
+                y_err[i] = np.nanstd(y_vals[i-ihwin:i+ihwin+1]) /\
+                    np.sqrt(y_vals[i-ihwin:i+ihwin+1].size)
             elif err_type == 'std':
-                y_err[i] = np.nanstd(y_avg[i-int(ihwin):i+int(ihwin)+1])
+                y_err[i] = np.nanstd(y_vals[i-ihwin:i+ihwin+1])
             else:
                 raise Exception('Error type provided is not understoud. Please select one of: sem, std')
-
+    
+    y_avg[:s_bin] = y_vals[:s_bin]
+    y_avg[e_bin:] = y_vals[e_bin:]
+    
     return(y_avg, y_err)
 
-def sliding_average_2D_fast(z_vals, y_vals, y_sm_lims, y_sm_win, expo = None, buf = []):
+def sliding_average_2D_fast_old(z_vals, y_vals, y_sm_lims, y_sm_win, expo = None):
     
     win = int(1E-3 * y_sm_win  / (y_vals[1] - y_vals[0]))
     
@@ -181,8 +268,7 @@ def sliding_average_2D_fast(z_vals, y_vals, y_sm_lims, y_sm_win, expo = None, bu
     s_bin = np.where(y_vals >= y_sm_lims[0])[0][0]
     e_bin = np.where(y_vals <= y_sm_lims[1])[0][-1]
     
-    if isinstance(buf, list):
-        buf = int(np.floor(win / 2.))
+    buf = int(np.floor(win / 2.))
 
     s_buf = s_bin + buf
     e_buf = e_bin - buf